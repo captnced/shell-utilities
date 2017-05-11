d7f244381f86ff05938502310e7884a3aa8ffaf7
Cedric Hamelin <cedric.m.hamelin@gmail.com>
2017-05-11 14:11:44 +0200

    try compile static ffmpeg

--- compile-ffmpeg.sh
+++ compile-ffmpeg.sh
@@ -141,6 +141,7 @@ PATH="$BINDIR:$PATH" PKG_CONFIG_PATH="$INSTALL_BUILD/lib/pkgconfig" ./configure 
   --prefix="$INSTALL_BUILD" \
   --extra-cflags="-I$INSTALL_BUILD/include" \
   --extra-ldflags="-L$INSTALL_BUILD/lib" \
+  --pkg-config-flags="--static" \
   --bindir="$BINDIR" \
   --enable-gpl \
   --enable-libass \
